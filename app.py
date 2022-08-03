5614ef02831e285b64bb8792d85e9ae99c0990af
Stephanie Romão <72523599+StephanieRomao@users.noreply.github.com>
2022-08-02 21:12:40 -0300

    Atualização

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ imagem = Image.open('CapaDoData.jpg')
 st.image(imagem)
 
 st.title('Jornada Além dos Dados')
-st.text('Aqui você encontrará uma analise dos sentimentos dos usuários do Twitter em')
-st.text('relação aos candidatos a presidência do ano de 2022!')
+st.text('Aqui você encontrará uma analise dos sentimentos dos usuários do Twitter em relação aos candidatos a presidência do ano de 2022!')
 
 df = pd.read_csv('Etapa_ 4_dataframe_apresentação.csv')
 
@@ -19,3 +18,17 @@ df_candidato_selecionado = df[df['Candidato'].isin(selecionar_candidato)]
 st.write('Conjunto de Dados ' + str(df_candidato_selecionado.shape[0]) + ' linhas e ' + '6' + ' colunas.')
 
 st.dataframe(df_candidato_selecionado)
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+    
+st.balloons()
